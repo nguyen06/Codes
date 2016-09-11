62520609dbe29551bc07456cad4c893f1759a381
NgocThinhNguyen <ngocthinhnguyen@NgocThinhNguyen.local>
2016-09-11 14:47:57 -0700

    save arezzo files

--- Codes/Python/thinkpython/walk.py
+++
@@ -1,9 +0,0 @@
-import os
-def walk(dirname):
-	for name in os.listdir(dirname):
-		path = os.path.join(dirname,name)
-		if os.path.isfile(path):
-			print path
-		else:
-			walk(path)
-walk('swampy-2.1.7')
