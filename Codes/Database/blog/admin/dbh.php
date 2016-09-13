117022851efa760e3594348d2f9b92f2693bdc33
NgocThinhNguyen <ngocthinhnguyen@NgocThinhNguyen.local>
2016-09-12 22:57:55 -0700

    what I changed

--- Codes/Database/blog/admin/dbh.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-//This file is only for chaecking connection if established between database and front end
-$conn =mysqli_connect("localhost","root", "","blog");
-if(!$conn){
-	die("Connection Failed: ".mysqli_connect_error());
-}
--- Codes/Database/blog/includes/db_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	$db = new mysqli('localhost', 'root', '','blog') or die('error with connection');
-
-?>
